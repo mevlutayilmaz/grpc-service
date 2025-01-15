bc3792070d92b300fbe4526da9bff08c1147a160
Mevlüt Akif Yılmaz <yilmaz.akif2525@gmail.com>
2025-01-15 14:56:59 +0300

    Unary method.

---
+++ GrpcClient/Program.cs
@@ -0,0 +1,17 @@
+﻿using Grpc.Net.Client;
+using GrpcClient;
+
+GrpcChannel channel = GrpcChannel.ForAddress("https://localhost:7219");
+var messageClient = new Message.MessageClient(channel);
+
+#region Unary Method
+MessageResponse response = await messageClient.UnaryCallAsync(new()
+{
+    Name = "Mevlüt",
+    Message = "Hello World!"
+});
+Console.WriteLine(response.Message);
+#endregion
+
+
+Console.Read();
---
+++ GrpcServer/Services/MessageService.cs
@@ -0,0 +1,20 @@
+﻿using Grpc.Core;
+
+namespace GrpcServer.Services
+{
+    public class MessageService : Message.MessageBase
+    {
+        private readonly ILogger<MessageService> _logger;
+        public MessageService(ILogger<MessageService> logger)
+        {
+            _logger = logger;
+        }
+
+        public override Task<MessageResponse> UnaryCall(MessageRequest request, ServerCallContext context)
+        {
+            Console.WriteLine($"Message: {request.Message} | Name: {request.Name}");
+            MessageResponse response = new() { Message = "Message received successfully.." };
+            return Task.FromResult(response);
+        }
+    }
+}
